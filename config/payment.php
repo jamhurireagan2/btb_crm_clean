bf0ccca270901d0f96b64d4bff3dec8b739c391d
jamhurireagan2 <jamhurireagan2@gmail.com>
2026-08-03 15:52:57 +0300

    Fixed payment functions path issues and simplified config

--- config/payment.php
+++ config/payment.php
@@ -1,55 +1,7 @@
 <?php
-// Payment Configuration
+// Payment Configuration - SIMPLE VERSION (no duplicates)
 
-// Currency
-define('CURRENCY', 'KES');
 define('CURRENCY_SYMBOL', 'KSh');
-
-// PayPal Settings
-define('PAYPAL_ENABLED', false);
-define('PAYPAL_MODE', 'sandbox');
-define('PAYPAL_CLIENT_ID', '');
-define('PAYPAL_SECRET', '');
-define('PAYPAL_BUSINESS_EMAIL', '');
-
-// M-Pesa Settings
-define('MPESA_ENABLED', false);
-define('MPESA_CONSUMER_KEY', '');
-define('MPESA_CONSUMER_SECRET', '');
-define('MPESA_PASSKEY', '');
-define('MPESA_SHORTCODE', '174379');
-define('MPESA_ENVIRONMENT', 'sandbox');
-
-// Company Details
-define('COMPANY_NAME', 'Client Management System');
-define('COMPANY_EMAIL', 'info@btbinsurance.com');
-define('COMPANY_PHONE', '0712345678');
-define('SITE_URL', 'https://client-managent-ystem.page.gd');
-?><?php
-// Payment Configuration
-
-// Currency Settings
 define('CURRENCY', 'KES');
-define('CURRENCY_SYMBOL', 'KSh');
-
-// PayPal Settings (disabled by default)
-define('PAYPAL_ENABLED', false);
-define('PAYPAL_MODE', 'sandbox');
-define('PAYPAL_CLIENT_ID', '');
-define('PAYPAL_SECRET', '');
-define('PAYPAL_BUSINESS_EMAIL', '');
-
-// M-Pesa Settings (disabled by default)
-define('MPESA_ENABLED', false);
-define('MPESA_CONSUMER_KEY', '');
-define('MPESA_CONSUMER_SECRET', '');
-define('MPESA_PASSKEY', '');
-define('MPESA_SHORTCODE', '174379');
-define('MPESA_ENVIRONMENT', 'sandbox');
-
-// Company Details
-define('COMPANY_NAME', 'Client Management System');
-define('COMPANY_EMAIL', 'info@btbinsurance.com');
-define('COMPANY_PHONE', '0712345678');
 define('SITE_URL', 'https://client-managent-ystem.page.gd');
 ?>
